620f5e2f83ace31068665fe93e3fefa31ed255a6
George <thebrainycraft@gmail.com>
2019-02-24 21:12:34 +0200

    Imported sprites & upgraded consumables and obstacles

--- Assets/Scripts/Consumable.cs
+++ Assets/Scripts/Consumable.cs
@@ -8,16 +8,48 @@ public class Consumable : MonoBehaviour
 
 	public bool needsReset = false;
 
+	[SerializeField] Sprite armorSprite = null;
+	[SerializeField] Sprite invincibleSprite = null;
+	[SerializeField] Sprite resetCameraSprite = null;
+
+	private SpriteRenderer currentRenderer = null;
+	private BoxCollider2D boxCollider = null;
+
 	private void Start()
 	{
 		consumableType = (ConsumableType)Random.Range(0, 2);
+
+		currentRenderer = GetComponent<SpriteRenderer>();
+		boxCollider = GetComponent<BoxCollider2D>();
 	}
 
 	private void FixedUpdate()
 	{
-		if(needsReset)
+		if(needsReset || Player.Instance.transform.position.y > transform.position.y + 12f)
 		{
-			consumableType = (ConsumableType)Random.Range(0, 2);
+			consumableType = (ConsumableType)Random.Range(0, (int)ConsumableType.COUNT);
+			switch (consumableType)
+			{
+				case ConsumableType.AddArmor:
+					currentRenderer.sprite = armorSprite;
+					boxCollider.size = new Vector2(0.52f, 0.51f);
+					boxCollider.offset = new Vector2(-0.01f, 0.03f);
+					break;
+				case ConsumableType.Invincible:
+					currentRenderer.sprite = invincibleSprite;
+					boxCollider.size = new Vector2(0.43f, 0.54f);
+					boxCollider.offset = new Vector2(-0.04f, 0.09f);
+					break;
+				case ConsumableType.ResetCamera:
+					currentRenderer.sprite = resetCameraSprite;
+					boxCollider.size = new Vector2(0.44f, 0.39f);
+					boxCollider.offset = new Vector2(0f, 0.03f);
+					break;
+				default:
+					Debug.Log("Not a consumable");
+					break;
+			}
+
 			needsReset = false;
 		}
 	}
@@ -27,5 +59,6 @@ public enum ConsumableType
 {
 	Invincible,
 	ResetCamera,
-	AddArmor
+	AddArmor,
+	COUNT
 }
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
 	[SerializeField] float removeTime = 25f;
+	[SerializeField] Sprite[] obstacleSprites = null;
+
+	private void Start()
+	{
+		SpriteRenderer currentSprite = GetComponent<SpriteRenderer>();
+		currentSprite.sprite = obstacleSprites[Random.Range(0, obstacleSprites.Length)];
+
+		transform.localScale = new Vector2(Random.Range(3f, 4f), Random.Range(3f, 4f));
+	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
