918320b84f82c8a5edbd938853bce7d9ea478e85
George <thebrainycraft@gmail.com>
2019-03-09 21:34:59 +0200

    Added music & button sounds

---
+++ Assets/Core/AudioManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+	public static AudioManager Instance = null;
+
+	public AudioSource buttonAudioSource;
+	public AudioSource musicAudioSource;
+
+	[SerializeField] AudioClip[] mainMenuAudioClips = null;
+
+	private bool songNeeded = true;
+
+	private void Awake()
+	{
+		if(Instance == null) 
+		{
+			Instance = this;
+			DontDestroyOnLoad(this);
+			return;
+		}
+		Destroy(this);
+	}
+
+	// Start is called before the first frame update
+	private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+		if (songNeeded && musicAudioSource)
+		{
+			musicAudioSource.clip = mainMenuAudioClips[Random.Range(0, mainMenuAudioClips.Length)];
+			musicAudioSource.Play();
+			StartCoroutine(SwitchSong(musicAudioSource.clip.length));
+		}
+	}
+
+	IEnumerator SwitchSong(float clipLength)
+	{
+		songNeeded = false;
+		yield return new WaitForSeconds(clipLength);
+		songNeeded = true;
+	}
+}
--- Assets/Core/GameManager.cs
+++ Assets/Core/GameManager.cs
@@ -9,12 +9,20 @@ public class GameManager : MonoBehaviour
 {
 	public static GameManager Instance;
 
-	public bool gamePaused = false;
-
 	public float realTimeSpeed;
 
+	public bool gamePaused = false;
+	public bool retryButtonPressed = false;
+
 	[Header("Main Menu")]
+	public string[] quotes;
+
 	[SerializeField] GameObject defaultScreen = null;
+	[SerializeField] GameObject optionsScreen = null;
+
+	[SerializeField] Text quoteText = null;
+
+	private bool needQuote = true;
 
 	[Header ("Game")]
 	[SerializeField] Text scoreText = null;
@@ -22,8 +30,6 @@ public class GameManager : MonoBehaviour
 	[SerializeField] Image armorImage = null;
 	[SerializeField] Spawner obstacleSpawner = null;
 	[SerializeField] GameObject loseScreen = null;
-
-	public bool retryButtonPressed = false;
 
 	public void ResetGame()
 	{
@@ -42,29 +48,40 @@ public class GameManager : MonoBehaviour
 
 	public void RetryButtonPressed()
 	{
+		AudioManager.Instance.buttonAudioSource.Play();
 		retryButtonPressed = true;
 	}
 
 	public void QuitButtonPressed()
 	{
-		Debug.Log("Application quit");
+		AudioManager.Instance.buttonAudioSource.Play();
 		Application.Quit();
 	}
 
 	public void MainMenuButtonPressed()
 	{
+		AudioManager.Instance.buttonAudioSource.Play();
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 	}
 
 	public void StartButtonPressed()
 	{
+		AudioManager.Instance.buttonAudioSource.Play();
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 
 	public void OptionsButtonPressed()
 	{
-		Debug.Log("Options enabled");
+		AudioManager.Instance.buttonAudioSource.Play();
 		defaultScreen.SetActive(false);
+		optionsScreen.SetActive(true);
+	}
+
+	public void OptionsButtonExit()
+	{
+		AudioManager.Instance.buttonAudioSource.Play();
+		optionsScreen.SetActive(false);
+		defaultScreen.SetActive(true);
 	}
 
 	private void Awake()
@@ -77,16 +94,21 @@ public class GameManager : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		if(scoreText != null && armorImage != null && healthImage != null)
+		if (gamePaused)
+		{
+			ResetGame();
+		}
+
+		if (scoreText != null && armorImage != null && healthImage != null)
 		{
 			scoreText.text = "Score : " + Player.Instance.transform.position.y.ToString("f0");
 			armorImage.fillAmount = Player.Instance.armorCount / 10f;
 			healthImage.fillAmount = Player.Instance.health / 10f;
 		}
 
-		if (gamePaused)
+		if (quoteText != null && quotes.Length != 0f && needQuote)
 		{
-			ResetGame();
+			StartCoroutine(SwitchQuote());
 		}
 
 		if (Player.Instance != null)
@@ -96,5 +118,13 @@ public class GameManager : MonoBehaviour
 		{
 			realTimeSpeed = 2f * Time.fixedDeltaTime;
 		}
+	}
+
+	IEnumerator SwitchQuote()
+	{
+		needQuote = false;
+		quoteText.text = quotes[Random.Range(0, quotes.Length)];
+		yield return new WaitForSeconds(5f);
+		needQuote = true;
 	}
 }
